32058d265c754d1cdfe442c434cea20a5c3b758b
crwambold <crwambold@gmail.com>
2021-03-24 16:36:33 -0400

    Added store grouping and JSON output

--- BoozeFinder/Program.cs
+++ BoozeFinder/Program.cs
@@ -17,24 +17,35 @@ namespace BoozeFinder
 
 		static void Main(string[] args)
 		{
-
-            var goodBooze = GetBooze.scrapeLimitedAvailability();
+			var storesWithBooze = new Dictionary<string, string>();
+			var goodBooze = GetBooze.scrapeLimitedAvailability();
 
 
 			foreach (var booze in goodBooze)
 			{
 				var stores = GetBooze.WebRequest(booze.Value);
 
-				stores.ForEach(store => {
-					if(store.quantity > 0)
+				stores.ForEach(store =>
+				{
+					if (store.quantity > 0)
 					{
-						Console.WriteLine($"{booze.Key} has been found at {store.address}");
+						var storeFoundText = $"{store.quantity} bottle(s) of {booze.Key} are available at {store.address}";
+						var storeId = store.storeId;
+						var storeInfo = $"{store.address}, Store ID:[{store.storeId}]";
+
+						storesWithBooze.Add(storeFoundText, storeInfo);
 					}
-				
+
 				});
 
 			}
 
+			var sortedStores = storesWithBooze.GroupBy(x => x.Value)
+			.ToDictionary(x => x.Key, x => x.Select(i => i.Key).ToList());
+
+			var one = JsonConvert.SerializeObject(sortedStores);
+
+			Console.WriteLine(one);
 		}
 
 		private static List<Store> WebRequest(string productId)
@@ -42,17 +53,20 @@ namespace BoozeFinder
 
 			try
 			{
-				var client = new RestClient("https://www.abc.virginia.gov/webapi/inventory/storeNearby?storeNumber=82&productCode=" + productId + "&mileRadius=999&storeCount=5&buffer=1");
+
+				// TODO: Take peramater for storeNumber and mileRadius 
+
+				var client = new RestClient("https://www.abc.virginia.gov/webapi/inventory/storeNearby?storeNumber=82&productCode=" + productId + "&mileRadius=30&storeCount=5&buffer=1");
 				var request = new RestRequest("", DataFormat.Json);
 
 				var response = client.Get(request);
 				var content = response.Content.Trim();
 
-			    var product = JsonConvert.DeserializeObject<Root>(content).products[0];
+				var product = JsonConvert.DeserializeObject<Root>(content).products[0];
 
 				var stores = new List<Store>();
 				stores.Add(product.storeInfo);
-				if(product.nearbyStores != null)
+				if (product.nearbyStores != null)
 				{
 					stores.AddRange(product.nearbyStores);
 
@@ -89,13 +103,6 @@ namespace BoozeFinder
 				var regex = new Regex(@"(?<productName>.*)\W*\|\W*(?<productId>[\d]+)");
 				var productMatch = regex.Matches(product.InnerText)[0];
 
-				Console.WriteLine(productMatch.Groups["productName"].Value, productMatch.Groups["producId"].Value);
-
-
-
-				//var split = product.InnerText.Split("|");
-				//limitedProducts.Add(split[0].Trim(), split[1].Trim().re);
-
 				limitedProducts.Add(productMatch.Groups["productName"].Value, productMatch.Groups["productId"].Value);
 			}
 
@@ -104,49 +111,49 @@ namespace BoozeFinder
 		}
 
 
-        private class PhoneNumber
-        {
-            public string AreaCode { get; set; }
-            public string Prefix { get; set; }
-            public string LineNumber { get; set; }
-            public string FormattedPhoneNumber { get; set; }
-        }
-
-        private class Product
-        {
-            public string productId { get; set; }
-            public Store storeInfo { get; set; }
-            public List<Store> nearbyStores { get; set; }
-        }
-
-        private class Root
-        {
-            public List<Product> products { get; set; }
-        }
-
-
-        private class Store
+		private class PhoneNumber
 		{
-            public int storeId { get; set; }
-            public int quantity { get; set; }
-            public double distance { get; set; }
-            public double latitude { get; set; }
-            public double longitude { get; set; }
-            public string address { get; set; }
-            public PhoneNumber PhoneNumber { get; set; }
-            public string url { get; set; }
-            public string address1 { get; set; }
-            public string address2 { get; set; }
-            public string city { get; set; }
-            public string state { get; set; }
-            public string zip { get; set; }
-            public string hours { get; set; }
-            public string shoppingCenter { get; set; }
-            public int closeForEcommerce { get; set; }
-            public bool counterService { get; set; }
-            public bool wholesale { get; set; }
+			public string AreaCode { get; set; }
+			public string Prefix { get; set; }
+			public string LineNumber { get; set; }
+			public string FormattedPhoneNumber { get; set; }
+		}
 
-        }
+		private class Product
+		{
+			public string productId { get; set; }
+			public Store storeInfo { get; set; }
+			public List<Store> nearbyStores { get; set; }
+		}
 
-    }
+		private class Root
+		{
+			public List<Product> products { get; set; }
+		}
+
+
+		private class Store
+		{
+			public int storeId { get; set; }
+			public int quantity { get; set; }
+			public double distance { get; set; }
+			public double latitude { get; set; }
+			public double longitude { get; set; }
+			public string address { get; set; }
+			public PhoneNumber PhoneNumber { get; set; }
+			public string url { get; set; }
+			public string address1 { get; set; }
+			public string address2 { get; set; }
+			public string city { get; set; }
+			public string state { get; set; }
+			public string zip { get; set; }
+			public string hours { get; set; }
+			public string shoppingCenter { get; set; }
+			public int closeForEcommerce { get; set; }
+			public bool counterService { get; set; }
+			public bool wholesale { get; set; }
+
+		}
+
+	}
 }
